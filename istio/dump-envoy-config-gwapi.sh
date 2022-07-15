a9676a63a3cfcef026f894fdbd40e43719a8d333
Grant Spence <gspence@redhat.com>
2022-07-15 14:21:39 -0700

    Fix for gwapi config dumps

--- istio/dump-envoy-config-gwapi.sh
+++ istio/dump-envoy-config-gwapi.sh
@@ -4,9 +4,8 @@ POD=$(oc get -n gwapi pod --no-headers | grep -i gateway | awk '{print $1}')
 oc port-forward --address 127.0.0.1 -n gwapi pod/${POD} 15000:15000 &
 
 while true; do
-  curl http://localhost:15000 &> /dev/null && break
-  echo "Waiting for port forwarding..."
+  curl http://localhost:15000/config_dump && break
   sleep 1
 done
 
-xdg-open http://localhost:15000
+kill $(jobs -p)
