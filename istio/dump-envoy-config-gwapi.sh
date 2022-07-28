cd0f4e5c9e4f0d398fee1f82859c9d6e60f14457
Grant Spence <gspence@redhat.com>
2022-07-28 13:54:18 -0700

    Added more detailed readme for istio demo. Various updates

--- istio/dump-envoy-config-gwapi.sh
+++ istio/dump-envoy-config-gwapi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kill $(lsof -i :15000 | tail -1 | awk '{print $2}') &> /dev/null
-POD=$(oc get -n gwapi pod --no-headers | grep -i gateway | awk '{print $1}')
+POD=$(oc get -n gwapi pod --no-headers | grep -i gateway | awk '{print $1}' | head -1)
 oc port-forward --address 127.0.0.1 -n gwapi pod/${POD} 15000:15000 &
 
 while true; do
--- istio/test-all-routes.sh
+++ istio/test-all-routes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 hosts_gwapi="$(oc get httproute -n gwapi --no-headers -o custom-columns="route:.spec.hostnames[0]") $(oc get tlsroute -n gwapi --no-headers -o custom-columns="route:.spec.hostnames[0]")"
-hosts_istio="$(oc get httproute -n istio-system --no-headers -o custom-columns="route:.spec.hostnames[0]") $(oc get tlsroute -n istio-system --no-headers -o custom-columns="route:.spec.hostnames[0]")"
+hosts_istio="$(oc get VirtualService -n istioapi --no-headers -o custom-columns="route:.spec.hosts[0]")"
 
 for j in ${hosts_istio} ${hosts_gwapi}; do
   echo "##### $j #####"
@@ -11,7 +11,7 @@ for j in ${hosts_istio} ${hosts_gwapi}; do
     PROTO="https"
   fi
 
-  cmd="curl -k ${silent} -I ${PROTO}://${j}"
+  cmd="curl -k -sS -I ${PROTO}://${j}"
   echo $cmd
   if [[ "$1" == "-h" ]]; then
     $cmd
