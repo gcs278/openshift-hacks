decec1bbe43215669116083b78242c581ce4c390
Grant Spence <gspence@redhat.com>
2023-10-17 18:16:06 -0400

    GCP Bug reproducer

---
+++ OCPBUGS-13106/reproducer.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+waitForServiceIP() {
+  START1=$(date +%s.%N)
+  echo "Waiting for router-$1..."
+  while [[ "$(oc get service -n openshift-ingress router-$1 -o jsonpath={.status.loadBalancer.ingress[0].ip})" == "" ]]; do
+    sleep 5
+  done
+  END1=$(date +%s.%N)
+  DIFF1=$(echo "$END1 - $START1" | bc)
+  
+  echo "Router-${1} took $DIFF1"
+}
+
+ICS=15
+for i in $(seq 1 $ICS); do
+  domain=$(oc get dnses cluster -o jsonpath={.spec.baseDomain})
+  export test_domain="demo${i}.${domain}"
+
+  oc apply -f - <<EOF
+apiVersion: operator.openshift.io/v1
+kind: IngressController
+metadata:
+  name: demo${i}
+  namespace: openshift-ingress-operator
+spec:
+  domain: $test_domain
+  replicas: 1
+  endpointPublishingStrategy:
+    type: LoadBalancerService
+    loadBalancer:
+      DNSManagementPolicy: Managed
+      scope: External
+  nodePlacement:
+    nodeSelector:
+      matchLabels:
+        node-role.kubernetes.io/worker: ""
+EOF
+
+done
+
+START=$(date +%s.%N)
+
+for i in $(seq 1 $ICS); do
+  waitForServiceIP demo${i} &
+done
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "Total Time: $DIFF"
+
+wait $(jobs -p)
+
+echo "Cleaning up..."
+for i in $(seq 1 $ICS); do
+  oc delete -n openshift-ingress-operator demo${i} &
+done
+
+wait $(jobs -p)
